1359861015d3a6eea461de50809cbb7444116943
codedubs <35834450+codedubs@users.noreply.github.com>
2018-08-31 02:47:09 -0700

    Checkpoint 4 components: completed

---
+++ react-to-do/src/components/ToDo.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react';
+
+
+class Todo extends Component {
+
+  render() {
+    return (
+      <li>a todo will go here</li>
+    );
+  }
+}
+
+
+export default ToDo;
